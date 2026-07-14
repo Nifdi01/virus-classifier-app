c612b113e9a201d069dff49827031b7731e0aae2
Nifdi Guliyev <nifdiguliyev@gmail.com>
2026-07-14 22:47:10 +0400

    Added a hero svg file to the home page for better UI

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,20 +7,20 @@ export default function Home() {
     <section className="text-gray-600 body-font">
       <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
         <img
-          className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+          className="lg:w-1/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
           alt="Nucleotide sequence classifier banner"
-          src="https://dummyimage.com/720x600"
+          src="/hero.svg"
         />
         <div className="text-center lg:w-2/3 w-full">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
             Nucleotide sequence classifier
           </h1>
-          {/* <p className="mb-8 leading-relaxed"> */}
-          {/*   Classify viral sequences with machine learning models quickly and */}
-          {/*   efficiently. Process individual items instantly or upload dataset */}
-          {/*   records in batch mode to evaluate large genomic sequences */}
-          {/*   seamlessly. */}
-          {/* </p> */}
+          <p className="mb-8 leading-relaxed">
+            Classify viral sequences with machine learning models quickly and
+            efficiently. Process individual items instantly or upload dataset
+            records in batch mode to evaluate large genomic sequences
+            seamlessly.
+          </p>
           <div className="flex justify-center">
             <button
               onClick={() => navigate("/processor")}
