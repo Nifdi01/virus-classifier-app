085d6a470045679fc8788584c40440dd70276085
Nifdi Guliyev <nifdiguliyev@gmail.com>
2026-07-15 16:08:21 +0400

    Added Footer and a Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Processor from "./pages/Processor";
 import "./index.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/processor" element={<Processor />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+const LINKS = [
+  { to: "/", label: "Home" },
+  { to: "/processor", label: "Processor" },
+  // { to: "/history", label: "History" },
+];
+
+export default function Navbar() {
+  return (
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 md:mb-0">
+          <img
+            className="w-15 h-15 text-white rounded-full"
+            src="/icons.svg"
+          ></img>
+          <span className="ml-3 text-xl">Virus Classifier</span>
+        </a>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          {LINKS.map((link) => {
+            return (
+              <Link
+                key={link.to}
+                to={link.to}
+                className={"mr-5 hover:text-gray-900"}
+              >
+                {link.label}
+              </Link>
+            );
+          })}
+        </nav>
+      </div>
+    </header>
+  );
+}
