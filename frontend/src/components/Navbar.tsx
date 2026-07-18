5a5d1429191d7c34b52327b5bc9ac6222bf5b405
Nifdi Guliyev <nifdiguliyev@gmail.com>
2026-07-18 14:05:12 +0400

    modified UI and filled empty hyperlinks

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,14 +3,17 @@ import { Link } from "react-router-dom";
 const LINKS = [
   { to: "/", label: "Home" },
   { to: "/processor", label: "Processor" },
-  // { to: "/history", label: "History" },
+  { to: "/history", label: "History" },
 ];
 
 export default function Navbar() {
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 md:mb-0">
+        <a
+          href="/"
+          className="flex title-font font-medium items-center text-gray-900 md:mb-0"
+        >
           <img
             className="w-12 h-12 text-white rounded-full"
             src="/icons.svg"
